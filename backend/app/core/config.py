f257382fa72cc7600e935f0c257fa599aa98dfcf
Thassanawalai <Baitouy2547@gmail.com>
2026-09-26 10:34:47 +0000

    HAPAG tem

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,11 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
-
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[
+            "https://5173-cs-722e0dc1-6d35-463a-bcc8-1cafec142b66.cs-asia-southeast1-palm.cloudshell.dev"
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
--- backend/app/templates/HAPAG_templete.py
+++ backend/app/templates/HAPAG_templete.py
@@ -1,21 +1,21 @@
 TEMPLATE = {
 
     "booking_no": {
-        "anchor_text": "",
+        "anchor_text": "Carrier’s Reference:",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
-        "offset_y": 5,
-        "width": 80,
+        "offset_x": 0,
+        "offset_y": 0,
+        "width": 15,
         "height": 5
     },
 
     "shipper": {
         "anchor_text": "Shipper:",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
+        "offset_x": 0,
         "offset_y": 0,
-        "width": 280,
-        "height": 60
+        "width": 265,
+        "height": 80
     },
 
     # "shipper_2": {
@@ -30,55 +30,55 @@ TEMPLATE = {
     "consignee": {
         "anchor_text": "Consignee:",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
+        "offset_x": 0,
         "offset_y": 0,
-        "width": 280,
-        "height": 55
+        "width": 265,
+        "height": 80
     },
 
     "notify_party": {
         "anchor_text": "Notify Address (Carrier not responsible for failure to notify):",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
-        "offset_y": 5,
-        "width": 250,
-        "height": 60
+        "offset_x": 0,
+        "offset_y": 0,
+        "width": 265,
+        "height": 80
     },
 
-    "pre_carriage_by": {
-        "anchor_text": "-",
-        "anchor_point": "bottom_left",
-        "offset_x": -5,
-        "offset_y": 0,
-        "width": 120,
-        "height": 12
-    },
+    #"pre_carriage_by": {
+    #    "anchor_text": "-",
+    #    "anchor_point": "bottom_left",
+    #    "offset_x": -5,
+    #    "offset_y": 0,
+    #    "width": 120,
+    #    "height": 12
+    #},
 
     "place_of_receipt": {
         "anchor_text": "Place of Receipt:",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
+        "offset_x": 0,
         "offset_y": 0,
-        "width": 130,
-        "height": 12
+        "width": 265,
+        "height": 65
     },
 
     "port_of_loading": {
         "anchor_text": "Port of Loading:",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
+        "offset_x": 0,
         "offset_y": 0,
-        "width": 130,
-        "height": 12
+        "width": 265,
+        "height": 10
     },
 
     "vessel": {
         "anchor_text": "Vessel(s):",
         "anchor_point": "bottom_left",
-        "offset_x": -5,
+        "offset_x": 0,
         "offset_y": 0,
-        "width": 130,
-        "height": 12
+        "width": 265,
+        "height": 10
     },
 
     "port_of_discharge": {
@@ -86,8 +86,17 @@ TEMPLATE = {
         "anchor_point": "bottom_left",
         "offset_x": -5,
         "offset_y": 0,
-        "width": 130,
-        "height": 12
+        "width": 265,
+        "height": 10
+    },
+
+    "voyage_no": {
+        "anchor_text": "Voyage-No.:",
+        "anchor_point": "bottom_left",
+        "offset_x": 0,
+        "offset_y": 0,
+        "width": 60,
+        "height": 10
     },
 
     "place_of_delivery": {
@@ -103,28 +112,28 @@ TEMPLATE = {
         "anchor_text": "Container Nos., Seal Nos.; Marks and Nos.",
         "anchor_point": "bottom_left",
         "offset_x": 0,
-        "offset_y": 50,
+        "offset_y": 100,
         "width": 200,
         "height": 100
     },
 
-    "quantity": {
-        "anchor_text": "QUANTITY",
+    "description_of_good": {
+        "anchor_text": "Number and Kind of Packages, Description of Goods",
         "anchor_point": "bottom_left",
-        "offset_x": -20,
-        "offset_y": 20,
-        "width": 60,
-        "height": 40
+        "offset_x": 0,
+        "offset_y": 0,
+        "width": 260,
+        "height": 100
     },
 
-    "description_of_good": {
-        "anchor_text": "DESCRIPTION OF GOODS",
-        "anchor_point": "bottom_left",
-        "offset_x": -80,
-        "offset_y": 15,
-        "width": 200,
-        "height": 75
-    },
+    #"description_of_good": {
+    #    "anchor_text": "DESCRIPTION OF GOODS",
+    #    "anchor_point": "bottom_left",
+    #    "offset_x": -80,
+    #    "offset_y": 15,
+    #    "width": 200,
+    #    "height": 75
+    #},
 
     "gross_weight": {
         "anchor_text": "Gross Weight:",
@@ -150,6 +159,14 @@ TEMPLATE = {
         "offset_y": 0,
         "width": 300,
         "height": 15
+    },
+    "movement": {
+        "anchor_text": "Movement:",
+        "anchor_point": "top_right",
+        "offset_x": 0,
+        "offset_y": 0,
+        "width": 180,
+        "height": 10
     },
 
 }
